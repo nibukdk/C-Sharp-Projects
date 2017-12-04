9a6d0fcf11db6d7b00bdac43254727cef20cca9e
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-12-04 12:05:10 +0200

    Basic Mathematical Calculation

---
+++ BasicCalculation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class BasicCalculation
+
+{
+
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Anna kaksi lukua:");
+
+        string usrInput1 = Console.ReadLine();
+        string usrInput2 = Console.ReadLine();
+
+
+        float num1 = float.Parse(usrInput1);
+        float num2 = float.Parse(usrInput2);
+
+
+        float sum = num1 + num2;
+        float sub = num1 - num2;
+        float prod = num1 * num2;
+        float div = num1 / num2;
+
+        Console.WriteLine(num1 + "+" + num2 + "=" + sum);
+        Console.WriteLine(num1 + "-" + num2 + "=" + sub);
+        Console.WriteLine(num1 + "*" + num2 + "=" + prod);
+        Console.WriteLine(num1 + "/" + num2 + "=" + div);
+
+    }
+}
